471c57be21d1ef75214311f2c96ef964304dc014
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-08-23 15:12:39 +0900

    Add some tests for RawArticle.

--- domain/entities/rawArticle.test.ts
+++ domain/entities/rawArticle.test.ts
@@ -1,5 +1,12 @@
-import { assert } from "https://deno.land/std@0.152.0/testing/asserts.ts";
-import { describe, it } from "https://deno.land/std@0.152.0/testing/bdd.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+import {
+  beforeAll,
+  describe,
+  it,
+} from "https://deno.land/std@0.152.0/testing/bdd.ts";
 
 import { RawArticle } from "./rawArticle.ts";
 
@@ -65,5 +72,40 @@ hogehoge
 `;
     const rawArticle = new RawArticle(filePath, fileContent);
     assert(!rawArticle.isValid());
+  });
+});
+
+describe("Test: RawArticle.get*()", () => {
+  let rawArticle: RawArticle;
+  beforeAll(() => {
+    const filePath = "articles/test-05.md";
+    const fileContent = `-----
+title: This is a test article.
+tags: [{"name": "test", "versions": ["0.0.0"]}]
+private: true
+-----
+
+# First Paragraph
+
+hogehoge
+`;
+    rawArticle = new RawArticle(filePath, fileContent);
+    assert(rawArticle.isValid());
+  });
+
+  it("Test: getCode", () => {
+    assertEquals(rawArticle.getCode(), "test-05");
+  });
+
+  it("Test: getTitle", () => {
+    assertEquals(rawArticle.getTitle(), "This is a test article.");
+  });
+
+  it("Test: getTags", () => {
+    assertEquals(rawArticle.getTags(), [{ name: "test", versions: ["0.0.0"] }]);
+  });
+
+  it("Test: getPrivate", () => {
+    assertEquals(rawArticle.getPrivate(), true);
   });
 });
